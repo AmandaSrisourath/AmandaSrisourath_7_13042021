602b6a502c5701ac8a939d9effe0875095195d69
Amandozz <amanda.srisou@gmail.com>
2021-04-14 16:06:41 +0200

    add dropdown

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-import 'bootstrap'
+import 'bootstrap';
+
+const dropdown = document.querySelectorAll(".dropdown-input");
+
+dropdown.forEach((input) => {
+    let dropdownMenu;
+    input.addEventListener("focusin", function (event) {
+        let nextElement = input;
+        while (nextElement) {
+            nextElement = nextElement.nextElementSibling;
+            if (nextElement.classList.contains("dropdown-menu")) {
+                nextElement.classList.add("show");
+                dropdownMenu = nextElement;
+                break;
+            }
+        }
+    });
+
+    input.addEventListener("focusout", function (event) {
+        dropdownMenu.classList.remove("show");
+    });
+});
+
+const openDropdown = document.querySelector("#dropdown-btn");
+openDropdown.click();
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
     mode: "production",
-    entry: [],
+    entry: ["./src/index.js"],
     output: {
         path: path.resolve(__dirname, "public"),
         filename: "bundle.js",
