90ebd2d894af0de395411e1773ef1a7fe18c0df4
Amandozz <amanda.srisou@gmail.com>
2021-04-21 12:10:27 +0200

    add svg

--- public/bundle.js
+++ public/bundle.js
@@ -19828,7 +19828,7 @@ _recipes_js__WEBPACK_IMPORTED_MODULE_1__.default.forEach((recipe) => {
     ingredients.forEach((ingredient) => {
         const div = document.createElement("div");
         div.classList.add("ingredients");
-        div.innerHTML = `<div class="ingredients-quantity">${ingredient.ingredient}: ${ingredient.quantity} ${ingredient.unit || ''}</div>`;
+        div.innerHTML = `<div class="ingredients-quantity"><span>${ingredient.ingredient}:</span> ${ingredient.quantity} ${ingredient.unit || ''}</div>`;
         divIngredients.appendChild(div);
     });
 
@@ -19837,7 +19837,13 @@ _recipes_js__WEBPACK_IMPORTED_MODULE_1__.default.forEach((recipe) => {
                         <div class="recipe-content rounded p-4 mb-4">
                             <div class="name-time">
                                 <div class="recipe-name">${recipe.name}</div>
-                                <div class="time">&#128339 ${recipe.time} min</div>
+                                <div class="time">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+                                      <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+                                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
+                                    </svg>
+                                    ${recipe.time} min
+                                </div>
                             </div>
                             <div class="realisation">
                                 ${divIngredients.outerHTML}
